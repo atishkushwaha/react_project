a6cb1aae89abef3f4ae86a0aee48a21ed2408b07
atishkushwaha <atish.kushwaha@zopsmart.com>
2022-09-06 17:06:24 +0530

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
+import { Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+const Hats = () => (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/  >
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={Hats} />
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,25 +4,26 @@ import "./directory.style.scss"
 class Directory extends React.Component {
 
     constructor() {
-
         super();
-
         this.state = {
             sections: [
                 {
                     title:'hats',
                     imageUrl:'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f2863064-0abc-4afd-9280-0d9e402c880d/authentics-mens-track-jacket-HgQM4q.png',
-                    id:1
+                    id:1,
+                    linkUrl:'hats'
                 },
                 {
                     title:'jacket',
                     imageUrl:'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f2863064-0abc-4afd-9280-0d9e402c880d/authentics-mens-track-jacket-HgQM4q.png',
-                    id:2
+                    id:2,
+                    linkUrl:''
                 },
                 {
                     title:'sneakers',
                     imageUrl:'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f2863064-0abc-4afd-9280-0d9e402c880d/authentics-mens-track-jacket-HgQM4q.png',
-                    id:3
+                    id:3,
+                    linkUrl:'',
                 },
                 {
                     title:'Mens',
@@ -44,9 +45,9 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title,id,imageUrl,size}) =>{
-                        return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-                    })
+                    this.state.sections.map(({id,...otherSections}) =>(
+                    <MenuItem key={id} {...otherSections}/>
+                    ))
                 }
             </div>
         )
